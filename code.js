f2d05da007bac37c6f5565f4f34ef1ea24f6f34d
heckogeck0 <mbugg02@gmail.com>
2023-09-06 15:20:54 -0600

    first try at binary search

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
 function binarySearch(list, element) {
+    var left = 0;
+    var right = list.length - 1;
+    var middle;
+
+    while (left <= right) {
+        middle = Math.floor((left + right)/2)
+        if (list[middle] < element) left = middle + 1;
+        else if (list[middle] > element) right = middle - 1;
+        else return middle;
+    }
+
     return -1;
 }
+
+
+/**
+ * References:
+ * https://en.wikipedia.org/wiki/Binary_search_algorithm to understand 
+ * different possible procedures for binary search
+ */
